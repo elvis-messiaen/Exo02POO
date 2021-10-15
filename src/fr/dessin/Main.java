a1d3614dc9734d075f3cc8148f05cc825406828f
Elvis Messiaen <elvism72@gmail.com>
2021-10-15 12:14:39 +0200

    fini

--- src/fr/dessin/Main.java
+++ src/fr/dessin/Main.java
@@ -3,6 +3,9 @@ package fr.dessin;
 public class Main {
 
     public static void main(String[] args) {
-
+    Point2D p = new Point2D(4,34);
+    p.afficher();
+    p.translater(7,14);
+    p.afficher();
     }
 }
--- src/fr/dessin/Point2D.java
+++ src/fr/dessin/Point2D.java
@@ -1,4 +1,46 @@
 package fr.dessin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Point2D {
+    private int x;
+    private int y;
+
+    public Point2D () {
+
+    }
+
+    public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void afficher (){
+        List<Integer> afficher = new ArrayList<>();
+        afficher.add(this.x);
+        afficher.add(this.y);
+        System.out.println(afficher);
+    }
+    public void translater (int dX, int dY) {
+        this.x += dX;
+        this.y += dY;
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
