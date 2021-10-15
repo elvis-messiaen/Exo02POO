ac5e21e39a744f2521b4b05a7f72778a36992e44
Elvis Messiaen <elvism72@gmail.com>
2021-10-15 13:41:33 +0200

    Exercice 4

--- src/fr/dessin/Main.java
+++ src/fr/dessin/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
     Point2D p = new Point2D(4,34);
+    Point2D p2 = new Point2D(5,25);
     p.afficher();
     p.translater(7,14);
     p.afficher();
+    System.out.println(p);
     }
 }
--- src/fr/dessin/Point2D.java
+++ src/fr/dessin/Point2D.java
@@ -6,26 +6,33 @@ import java.util.List;
 public class Point2D {
     private int x;
     private int y;
+    public static int compteur;
 
-    public Point2D () {
+    public Point2D() {
 
     }
 
     public Point2D(int x, int y) {
         this.x = x;
         this.y = y;
+        compteur++;
     }
 
-    public void afficher (){
+    public void afficher() {
         List<Integer> afficher = new ArrayList<>();
         afficher.add(this.x);
         afficher.add(this.y);
         System.out.println(afficher);
     }
-    public void translater (int dX, int dY) {
+
+    public void translater(int dX, int dY) {
         this.x += dX;
         this.y += dY;
 
+    }
+
+    public static int getCompteur() {
+        return compteur;
     }
 
     public int getX() {
@@ -42,5 +49,10 @@ public class Point2D {
 
     public void setY(int y) {
         this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "compteur de nombre d'instance " + compteur;
     }
 }
